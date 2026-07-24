34b7dfb1c26f13643e6e84386e85639606dee1b8
Kaumini Samodhi <kauminisk123@gmail.com>
2026-07-24 21:53:01 +0530

    Add basename to Router for GitHub Pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/portfolio">   {/* ← ADD THIS */}
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
